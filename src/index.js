1d32cc44f26cd14f1c394577a793d5c7ac3deeae
tanjiahao24 <2476987169@qq.com>
2023-04-06 15:59:07 +0800

    setup framework

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
+import '@/assets/styles/index.less'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('ddd')
+let p = fetch('/api/news_latest')
+p.then(res => res.json())
+    .then(res => console.log(res))
 root.render(
     <App />
 );
